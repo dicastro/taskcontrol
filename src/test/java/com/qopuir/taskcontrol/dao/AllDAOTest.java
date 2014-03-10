2b2dc39c3c8867a5ce3bbc170244c00b7b49decc
dcastro <dcastro@diego-HP-ENVY-17-Notebook-PC>
2014-03-11 00:44:15 +0100

    - Adding Spring Batch (not working yet)

---
+++ src/test/java/com/qopuir/taskcontrol/dao/AllDAOTest.java
@@ -0,0 +1,10 @@
+package com.qopuir.taskcontrol.dao;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ControlDAOTest.class, ControlScheduleDAOTest.class, ControlUserDAOTest.class, UserDAOTest.class })
+public class AllDAOTest {
+}
